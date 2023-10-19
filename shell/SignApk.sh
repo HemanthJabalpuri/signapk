03ee8b62a9a96fb0b872b5a89aafc2a8537d0588
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2023-10-19 13:32:46 +0530

    update shell source - check if comment is already there - check arguments before proceeding

--- shell/SignApk.sh
+++ shell/SignApk.sh
@@ -10,6 +10,15 @@
 # Author : HemanthJabalpuri
 # Date   : 16th October 2023
 
+abort() {
+  echo "$1"
+  exit 1
+}
+
+if [ $# -lt 2 ]; then
+  abort "Usage: sh SignApk.sh in.zip out.zip"
+fi
+
 tmpd="$PWD"; [ "$PWD" = "/" ] && tmpd=""
 case "$0" in
   /*) cdir="$0";;
@@ -22,9 +31,26 @@ o="$2"
 PKEY="$cdir/testkey.pk8"
 SBT="$cdir/testkey.sbt"
 
-# copy whole file except last 2 bytes
+getData() {
+  dd if="$f" status=none iflag=skip_bytes,count_bytes bs=4096 skip=$1 count=$2
+}
+
+getByte() {
+  getData $1 1 | od -A n -t x1 | tr -d " "
+}
+
 fsize=$(stat -c "%s" "$f")
-dd if="$f" of="$o" status=none iflag=count_bytes bs=4096 count=$((fsize-2))
+
+# check if already comment is there
+tail1=$(getByte $((fsize-22)))
+tail2=$(getByte $((fsize-21)))
+tail3=$(getByte $((fsize-20)))
+if [ $tail1 != "50" -o $tail2 != "4b" -o $tail3 != "05" ]; then
+  abort "zip data already has an archive comment"
+fi
+
+# copy whole file except last 2 bytes
+getData 0 $((fsize-2)) > "$o"
 
 sign="$(openssl dgst -sha1 -hex -sign "$PKEY" "$o")"
 sign="$(echo "$sign" | cut -d "=" -f 2 | tr -d " " | sed -e 's/../\\x&/g')"
